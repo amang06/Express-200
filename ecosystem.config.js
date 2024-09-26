6fd9a80bba8ec5fbf9b23628f46d308f6b3f1576
amang06 <aman@skynetiks.com>
2024-09-26 13:11:20 +0530

    a

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,13 @@ module.exports = {
     apps: [
       {
         name: 'express-200',
-        script: './node_modules/.bin/tsx',
+        script: 'node_modules/.bin/tsx',
         args: 'server.ts',
         watch: true,
+        interpreter: 'node',
         env_production: {
           NODE_ENV: 'production',
-          PORT: 6879,
-          // Add other environment variables here
+          PORT: 6879
         }
       }
     ]
