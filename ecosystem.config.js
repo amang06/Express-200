bd056741c22281ad56116fab1a8303d0c5e7e866
amang06 <aman@skynetiks.com>
2024-09-26 12:50:34 +0530

    ts-node

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'SkyFunnel-Warmup-Service',
-        script: './node_modules/.bin/tsx',
+        script: './node_modules/.bin/ts-node',
         args: 'server.ts',
         watch: true,
         env_production: {
