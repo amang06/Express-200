109239ddeab8d1c7afa316a11c5aa69b590ce257
amang06 <aman@skynetiks.com>
2024-09-26 10:35:04 +0530

    first commit

---
+++ server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); // Middleware to parse JSON bodies
+
+app.get("/", (req: any, res: any) => {
+  res.sendStatus(200);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
